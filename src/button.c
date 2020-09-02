b3bb7ad29373ee1ef3952a3e32a0af53c3fb1e3b
elijah <pipimi@tfwno.gf>
2020-09-02 00:09:18 -0700

    defines broken maybe idk

--- src/button.c
+++ src/button.c
@@ -13,30 +13,36 @@ void setupDButtons ( PORTD_BUTTONS *b )
   PORTD |= b->mask;
 
   for (int i = 0; i < 4; ++i) {
-    if ( BIT_CHECK(b->mask,i) )
-      BIT_SET(DDRB, b->B_LEDS[i]);
+    if ( b->mask & ( 1<<i) )
+      DDRB |= (1 << (b->B_LEDS[i]));
   }
 
   for (int i = 0; i < 4; ++i) {
-    if ( BIT_CHECK(b->mask,(i+4)) )
-      BIT_SET(DDRF, b->F_LEDS[i]);      
+    if ( b->mask & ( 1 << (i +4)) )
+      DDRF |= (1 << (b->F_LEDS[i]));
   }
 
 }
 
 uint8_t getDState( PORTD_BUTTONS *b, uint64_t currentTime ) 
 {
-  uint8_t currentState = b->mask & ~(PIND);
+  uint8_t raw_pinstate = PIND;
+  uint8_t currentState = b->mask & ~(raw_pinstate);;
   
   for ( int i = 0; i < 8; ++i ) {
-    if ( BIT_CHECK(b->isDebouncing,i) ) {
-      if ( currentTime - b->last_pressed[i] >= TIMER_VAL ) {
-        BIT_ASSIGN(b->state,i,(BIT_CHECK(currentState,i)));
+    if ( b->isDebouncing & (1<<i) ) {
+      if ( (currentTime - b->last_pressed[i]) >= TIMER_VAL ) {
+       if ( currentState & ( 1 << i ) ) {
+         b->state |= (1 << i);
+       }
+       else {
+         b->state &= ~(1 << i);
+       }
         b->isDebouncing &= ~(1<<i);
       }
     }
-    else if ( BIT_CHECK(currentState,i) != BIT_CHECK(b->state,i)  ) {
-      BIT_SET(b->isDebouncing,i);
+    else if ( (currentState & ( 1<< i)) != ((b->state) & ( 1 <<i))  ) {
+      b->isDebouncing |= ( 1 << i );
       b->last_pressed[i] = currentTime;
     }
   }
@@ -47,13 +53,21 @@ uint8_t getDState( PORTD_BUTTONS *b, uint64_t currentTime )
 void LED_D_toggle ( PORTD_BUTTONS *b ) 
 {
   for (int i = 0; i < 4; ++i) {
-    if ( BIT_CHECK(b->state,i) )
-      BIT_SET(PORTB, b->B_LEDS[i]);
+    if ( b->state & ( 1<<i) ){
+      PORTB |= (1 << b->B_LEDS[i]);
+    }
+    else {
+      PORTB &= ~(1 << b->B_LEDS[i]);
+    }
   }
 
   for (int i = 0; i < 4; ++i) {
-    if ( BIT_CHECK(b->state,(i+4)) )
-      BIT_SET(PORTF, b->F_LEDS[i]);      
+    if ( b->state & ( 1<< (i+4)) ){
+      PORTB |= (1 << b->B_LEDS[i]);
+    }
+    else {
+      PORTB &= ~(1 << b->B_LEDS[i]);
+    }
   }
 
 }
